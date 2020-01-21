09750cc7b7974ebb6538e0c2b3182ab96ca5f503
Canton Condes Alexandre <alexandre.canton-condes@etu.umontpellier.fr>
2020-01-21 14:27:58 +0100

    renommage

--- TER_L3.git/Stage-L2-CMI/PHP/test.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$valeur = "vooo";
-$p= "jjj";
-$l.= '<input type="radio" class="spacing" name= "'.$p. '" value="' .$valeur. '"' .'>' . $valeur ;
-
-?>
--- TER_L3.git/Stage-L2-CMI/ajout.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("connexion.php");
-connexion();
-$sql = "INSERT INTO mabase(Q1,Q2,Q3,Q5) ";
-$sql .= "VALUES('','".$_POST['Q1']."','".$_POST['Q2']."','".$_POST['Q3']."','".$_POST['Q4']."','".$_POST['Q5']."')";
-    
-mysql_query($sql) or die(mysql_error());
-?>
